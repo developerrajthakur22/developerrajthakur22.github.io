df14e5a0e09c1f8328bf689983749f2c51056d8f
Raj Thakur <rajt4900@gmail.com>
2023-07-25 15:58:33 +0530

    updated

---
+++ downloadbtn.js
@@ -0,0 +1,6 @@
+function openLink() {
+    // Replace 'https://example.com' with the actual URL you want to open
+    const linkUrl = 'https://drive.google.com/uc?id=1_zssm6sE0W1fJk4KYEIGTbH5m99fuCdG';
+    window.open(linkUrl, '_blank');
+    console.log("rhy")
+  }
