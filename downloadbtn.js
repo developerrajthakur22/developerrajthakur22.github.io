9835435728fced8c3a583a10934904f957564223
Raj Thakur <121308092+developerrajthakur22@users.noreply.github.com>
2024-04-19 16:49:20 +0530

    Update downloadbtn.js

--- downloadbtn.js
+++ downloadbtn.js
@@ -1,6 +1,6 @@
 function openLink() {
     // Replace 'https://example.com' with the actual URL you want to open
-    const linkUrl = 'https://drive.google.com/uc?id=1_zssm6sE0W1fJk4KYEIGTbH5m99fuCdG';
+    const linkUrl = 'https://drive.google.com/uc?export=download&id=1j7Tdr-18T02bVWTCrj_2nLWyxuKCMIRU';
     window.open(linkUrl, '_blank');
     console.log("rhy")
   }
